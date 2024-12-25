469b675af947d27e688119893b0889dbcc7c84c0
SDVoGo <abirettoni@gmail.com>
2024-12-25 12:48:36 +0100

    Dimuito il numero di cicli sbloccando i campi contestualmente alla mappatura della soluzione

--- script.js
+++ script.js
@@ -12,21 +12,25 @@ const caricaDatiCruciverba = async () => {
   }
 };
 
+// Carica la matrice e sblocca i campi che andranno valorizzati
 const caricaMatrice = (jsonSoluzione) => {
   jsonSoluzione.forEach(element => {
     if (element.orientamento == "orizzontale") {
       for (let i = 0; i < element.parola.length; i++) {
         soluzione[element.posizione.y][element.posizione.x + i] = element.parola[i];
+        document.querySelector(`[data-y='${element.posizione.y}'][data-x='${element.posizione.x + i}']`).removeAttribute("disabled")
       }
     } else {
       for (let i = 0; i < element.parola.length; i++) {
         soluzione[element.posizione.y + i][element.posizione.x] = element.parola[i];
+        document.querySelector(`[data-y="${element.posizione.y + i}"][data-x="${element.posizione.x}"]`).removeAttribute("disabled")
       }
     }
   });
 };
 
 const disegnaCruciverba = () => {
+
   // Ciclo per disegnare 77 input nella griglia
   for (let i = 0; i < 77; i++) {
     const input = document.createElement("input");
@@ -54,6 +58,6 @@ const disegnaCruciverba = () => {
 window.addEventListener("load", async () => {
   // Carica i dati e la matrice in modo sequenziale
   await caricaDatiCruciverba();
-  caricaMatrice(jsonSoluzione);
   disegnaCruciverba();
+  caricaMatrice(jsonSoluzione);
 });
